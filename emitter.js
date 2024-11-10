fdb1adc7013993480c626e5d7b0bd5c80385e1d6
postfm <72159645+postfm@users.noreply.github.com>
2024-11-10 18:33:40 +0300

    fix: learn https

--- emitter.js
+++ emitter.js
@@ -1,8 +1,14 @@
 const EventEmitter = require('events')
 const eventEmitter = new EventEmitter()
 
-eventEmitter.on('start', () => {
-  console.log('started')
+// eventEmitter.on('start', (number) => {
+//   console.log(`started ${number}`)
+// })
+
+// eventEmitter.emit('start', 23)
+
+eventEmitter.on('start', (start, end) => {
+  console.log(`started ${start} to ${end}`)
 })
 
-eventEmitter.emit('start')
+eventEmitter.emit('start', 1, 100)
--- server.js
+++ server.js
@@ -1,14 +1,66 @@
-const http = require('http')
+// const http = require('http')
 
-const hostname = '127.0.0.1'
-const port = 3000
+// const hostname = '127.0.0.1'
+// const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello, World\n')
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.end('Hello, World\n')
+// })
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}`)
+// })
+
+// const https = require('https')
+// const options = {
+//   hostname: 'flaviocopes.com',
+//   port: 443,
+//   path: '/todos',
+//   method: 'GET'
+// }
+
+// const req = https.request(options, (res) => {
+//   console.log(`statusCode: ${res.statusCode}`)
+//   res.on('data', (d) => {
+//     process.stdout.write(d)
+//   })
+// })
+
+// req.on('error', (error) => {
+//   console.log(error)
+// })
+
+// req.end()
+
+const https = require('https')
+
+const data = JSON.stringify({
+  todo: 'Buy the milk!'
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`)
+const options = {
+  hostname: 'flaviocopes.com',
+  port: 443,
+  path: '/todos',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  }
+}
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+  res.on('data', (d) => {
+    process.stdout.write(d)
+  })
 })
+
+req.on('error', (error) => {
+  console.log(error)
+})
+
+req.write(data)
+req.end()
